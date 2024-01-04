5a1ac9dcf3f081afaacbec402323a6e708cb83b7
Hardikmaind <maindhardik@gmail.com>
2024-01-04 19:30:28 +0530

    done changes to local setup

--- frontend/login_regis/src/api/api.jsx
+++ frontend/login_regis/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'; // Renamed the imported type
 
 const AxiosInstance = axios.create({
-  baseURL: 'http://192.168.0.117:8000/api/',
+  baseURL: 'http://127.0.0.1:8000/api/',
 });
 AxiosInstance.interceptors.request.use(async (request) => {
   const access_token = localStorage.getItem('token');
