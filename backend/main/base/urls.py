aaf681e0e4f566b434413bbddf40c04ae1ca5811
Hardikmaind <maindhardik@gmail.com>
2024-01-05 09:59:02 +0530

    change the name of the urls . done properly

--- backend/main/base/urls.py
+++ backend/main/base/urls.py
@@ -8,8 +8,8 @@ from django.urls import path
 
 urlpatterns = [
     path('register' , RegisterView.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('login/' , LoginView.as_view()),
+    path('token/' , LoginView.as_view()),
 ]
